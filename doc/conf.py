255fe7f80674b3c668688e4616c59cc16ee4a6fd
Ronald Oussoren <ronaldoussoren@mac.com>
2018-12-31 16:34:21 +0100

    Tweaks to the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -136,7 +136,7 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -147,7 +147,10 @@ html_static_path = ['_static']
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    '**': [ 'localtoc.html', 'links.html', 'donate.html', 'searchbox.html' ],
+}
+
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
