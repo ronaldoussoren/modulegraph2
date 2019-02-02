c6a54448d4498b77fa1031403a9e339c1a6643cb
Ronald Oussoren <ronaldoussoren@mac.com>
2019-02-02 22:40:34 +0100

    Move shared testing code to a test mixin

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0,
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named "sphinx.ext.*") or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.todo", "sphinx.ext.ifconfig", "sphinxcontrib.napoleon", "sphinx_autodoc_typehints"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.todo", "sphinx.ext.ifconfig", "sphinxcontrib.napoleon", "sphinx_autodoc_typehints", "sphinxcontrib.blockdiag"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
